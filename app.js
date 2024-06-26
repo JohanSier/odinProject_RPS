9a3028aa0df437f2ba2189d18c17e1794873a6cc
johanstevencrack <johanstevencrack@gmail.com>
2024-06-26 00:48:35 +0000

    Create playRound function
    The logic is to show when the human wins or loses and when the computer wins or loses

--- app.js
+++ app.js
@@ -26,3 +26,55 @@ function getHumanChoice() {
     return humanChoice;
 }
 
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+
+function playRound(humanChoice, computerChoice) {
+    
+  if(humanChoice.toLowerCase() != computerChoice.toLowerCase()){
+      /*---------------- WHEN THE HUMAN LOSES ----------------*/
+      if(humanChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "paper"){
+      computerScore += 1
+      console.log(`${userName} you lose! Paper beats Rock`)
+      console.log(`COMPUTER CHOICE: ${computerChoice} \n Current Score: COMPUTER: ${computerScore} ${userName.toUpperCase()}: ${humanScore}`)
+      }
+      
+       else if(humanChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "rock"){
+          computerScore += 1
+          console.log(`${userName} you lose! Rock beats Scissors`)
+          console.log(`COMPUTER CHOICE: ${computerChoice} \n Current Score: Computer: ${computerScore} ${userName.toUpperCase()}: ${humanScore}`)
+      }
+      
+      else if(humanChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "scissors"){
+          computerScore += 1
+          console.log(`COMPUTER CHOICE: ${computerChoice} \n ${userName} you lose! Scissors beats Paper`)
+          console.log(`Current Score: Computer: ${computerScore} ${userName.toUpperCase()}: ${humanScore}`)
+      }
+      
+            /*---------------- WHEN THE HUMAN WINS ----------------*/
+
+      
+      else if(humanChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock"){
+          humanScore += 1
+          console.log(`COMPUTER CHOICE: ${computerChoice} \n ${userName} you win! Paper beats Rock`)
+          console.log(`Current Score: Computer: ${computerScore} ${userName.toUpperCase()}: ${humanScore}`)
+      }
+      
+      else if(humanChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissors"){
+          humanScore += 1
+          console.log(`COMPUTER CHOICE: ${computerChoice} \n ${userName} you win! Rock beats Scissors`)
+          console.log(`Current Score: Computer: ${computerScore} ${userName.toUpperCase()}: ${humanScore}`)
+      }
+      
+      else if(humanChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "paper"){
+          humanScore += 1
+          console.log(`COMPUTER CHOICE: ${computerChoice} \n ${userName} you win! Scissors beats Paper`)
+          console.log(`Current Score: Computer: ${computerScore} ${userName.toUpperCase()}: ${humanScore}`)
+      }
+  }
+  else {
+      console.log(`COMPUTER CHOICE: ${computerChoice} \n${userName} it's a Tie!`)
+      console.log(`Current Score: \n Computer: ${computerScore} \n ${userName.toUpperCase()}: ${humanScore}`)
+  }
+}
